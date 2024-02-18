49696ae850bb2d6552cb009c63f3e87df03d3953
amin <amansouri3476@gmail.com>
2024-02-18 15:26:42 -0500

    add header

--- datamodule/augmentations.py
+++ datamodule/augmentations.py
@@ -1,4 +1,8 @@
-
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 
 PIL_to_th = lambda x: x  # transforms.PILToTensor()
